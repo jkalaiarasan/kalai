738f75c616cc0dace82bb1cbbcd890e06d6eb463
jkalaiarasan <60134661+jkalaiarasan@users.noreply.github.com>
2023-04-01 17:13:27 +0530

    by changed

--- src/app/page.js
+++ src/app/page.js
@@ -18,14 +18,14 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{' '}
-            <Image
+            {/* <Image
               src="/vercel.svg"
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
               height={24}
               priority
-            />
+            /> */} Nu Finc
           </a>
         </div>
       </div>
